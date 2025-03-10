f133b595defb36259c794ff0066a068ab308a5d0
ml11109 <leejhmalcolm@gmail.com>
2025-03-10 22:50:13 +0800

    Initial commit

---
+++ app/src/main/java/com/example/ica2_prep/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ica2_prep
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+    }
+}
