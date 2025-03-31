b916243a94d7623e3e01909c65ef1cb1be564255
ml11109 <leejhmalcolm@gmail.com>
2025-03-31 20:56:06 +0800

    Added ExpandingTextField

--- app/src/main/java/com/example/ica2_prep/composables/TextFields.kt
+++ app/src/main/java/com/example/ica2_prep/composables/TextFields.kt
@@ -1,6 +1,7 @@
 package com.example.ica2_prep.composables
 
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -11,13 +12,18 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -26,14 +32,116 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+
+/*
+// Text field with button beside that expands it when clicked
+// text field contracts to fit text when unfocused
+ExpandingTextField { text = it }
+ */
+
+@Composable
+fun ExpandingTextField(
+    modifier: Modifier = Modifier,
+    hint: String = "Enter text...",
+    width: Dp = 200.dp,
+    height: Dp = 54.dp,
+    textStyle: TextStyle = LocalTextStyle.current,
+    isButtonOnRight: Boolean = true,
+    showHintIfEmpty: Boolean = false,
+    onTextChanged: (String) -> Unit
+) {
+    val density = LocalDensity.current
+    val textMeasurer = rememberTextMeasurer()
+    val shouldShowHintIfEmpty = showHintIfEmpty && hint.isNotBlank()
+    var minWidth by remember { mutableStateOf(
+        if (!shouldShowHintIfEmpty) 0.dp else {
+            (with(density) {
+                textMeasurer.measure(hint, textStyle).size.width.toDp()
+            } + 32.dp).coerceAtMost(width)
+        }
+    ) }
+
+    var expanded by remember { mutableStateOf(false) }
+    val currentWidth by animateDpAsState(if (expanded) width else minWidth)
+    val alpha by animateFloatAsState(if (expanded) 1f else 0f)
+    var text by remember { mutableStateOf("") }
+    val focusRequester = remember { FocusRequester() }
+    val focusManager = LocalFocusManager.current
+
+
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = if (isButtonOnRight) Arrangement.Start else Arrangement.End
+    ) {
+        Box {
+            OutlinedTextField(
+                value = text.ifBlank { hint },
+                onValueChange = {},
+                singleLine = true,
+                textStyle = textStyle,
+                modifier = Modifier
+                    .width(currentWidth)
+                    .height(height)
+                    .alpha(1 - alpha),
+                colors = OutlinedTextFieldDefaults.colors(
+                    focusedBorderColor = Color.Transparent,
+                    unfocusedBorderColor = Color.Transparent
+                ),
+            )
+
+            OutlinedTextField(
+                value = text,
+                onValueChange = {
+                    text = it
+                    onTextChanged(it)
+                    minWidth = if (text.isBlank() && !shouldShowHintIfEmpty) 0.dp else {
+                        (with(density) {
+                            textMeasurer.measure(it.ifBlank { hint }, textStyle).size.width.toDp()
+                        } + 32.dp).coerceAtMost(width)
+                    }
+                },
+                placeholder = { Text(hint, style = textStyle) },
+                singleLine = true,
+                textStyle = textStyle,
+                modifier = Modifier
+                    .width(currentWidth)
+                    .height(height)
+                    .alpha(alpha)
+                    .focusRequester(focusRequester)
+                    .onFocusChanged { expanded = it.isFocused },
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus() })
+            )
+        }
+
+        IconButton(
+            onClick = {
+                expanded = !expanded
+                if (expanded) {
+                    focusRequester.requestFocus()
+                } else {
+                    focusManager.clearFocus()
+                }
+            }
+        ) {
+            Icon(Icons.Default.Edit, null)
+        }
+    }
+}
 
 /*
 // Search button that opens a search bar to the side
@@ -46,40 +154,41 @@ ExpandingSearchBarButton { contentModifier ->
  */
 
 @Composable
-fun ExpandingSearchBarButton(
+fun ExpandingBarButton(
     modifier: Modifier = Modifier,
     width: Dp = 200.dp,
     isButtonOnRight: Boolean = true,
-    content: @Composable (contentModifier: Modifier) -> Unit
+    icon: ImageVector = Icons.Default.Create,
+    content: @Composable (contentModifier: Modifier, expanded: Boolean) -> Unit
 ) {
-    var isExpanded by remember { mutableStateOf(false) }
-    val currentWidth by animateDpAsState(if (isExpanded) width else 0.dp)
+    var expanded by remember { mutableStateOf(false) }
+    val currentWidth by animateDpAsState(if (expanded) width else 0.dp)
     val focusRequester = remember { FocusRequester() }
     val focusManager = LocalFocusManager.current
 
     val contentModifier = Modifier
         .width(currentWidth)
         .focusRequester(focusRequester)
-        .onFocusChanged { if (!it.isFocused) isExpanded = false }
+        .onFocusChanged { if (!it.isFocused) expanded = false }
 
     Row(
         modifier = modifier,
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = if (isButtonOnRight) Arrangement.Start else Arrangement.End
     ) {
-        content(contentModifier)
+        content(contentModifier, expanded)
 
         IconButton(
             onClick = {
-                if (isExpanded) {
-                    focusManager.clearFocus()
-                } else {
+                expanded = !expanded
+                if (expanded) {
                     focusRequester.requestFocus()
+                } else {
+                    focusManager.clearFocus()
                 }
-                isExpanded = !isExpanded
             }
         ) {
-            Icon(Icons.Default.Search, "Search")
+            Icon(icon, null)
         }
     }
 }
@@ -95,8 +204,9 @@ fun BasicExpandingSearchBar(
     onTextChanged: (String) -> Unit
 ) {
     var text by remember { mutableStateOf("") }
+    val focusManager = LocalFocusManager.current
 
-    ExpandingSearchBarButton(modifier, width, isButtonOnRight) { contentModifier ->
+    ExpandingBarButton(modifier, width, isButtonOnRight, Icons.Default.Search) { contentModifier, _ ->
         Box(
             modifier = contentModifier,
             contentAlignment = Alignment.Center
@@ -108,6 +218,7 @@ fun BasicExpandingSearchBar(
                     BasicTextField(
                         value = hint,
                         onValueChange = {},
+                        singleLine = true,
                         enabled = false,
                         textStyle = textStyle.copy(color = Color.Gray),
                         modifier = Modifier.fillMaxWidth().height(height)
@@ -118,7 +229,9 @@ fun BasicExpandingSearchBar(
                     onValueChange = { text = it; onTextChanged(it) },
                     singleLine = true,
                     textStyle = textStyle,
-                    modifier = Modifier.fillMaxWidth().height(height)
+                    modifier = Modifier.fillMaxWidth().height(height),
+                    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                    keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus() })
                 )
             }
         }
@@ -136,15 +249,18 @@ fun OutlinedExpandingSearchBar(
     onTextChanged: (String) -> Unit
 ) {
     var text by remember { mutableStateOf("") }
+    val focusManager = LocalFocusManager.current
 
-    ExpandingSearchBarButton(modifier, width, isButtonOnRight) { contentModifier ->
+    ExpandingBarButton(modifier, width, isButtonOnRight, Icons.Default.Search) { contentModifier, _ ->
         OutlinedTextField(
             value = text,
             onValueChange = { text = it; onTextChanged(it) },
             placeholder = { Text(hint) },
             singleLine = true,
             textStyle = textStyle,
-            modifier = contentModifier.height(height)
+            modifier = contentModifier.height(height),
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+            keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus() })
         )
     }
 }
